b5af330f312767964046bf8f6a7cf2c43a27c312
BoBBruh1 <65122660103@g.lpru.ac.th>
2023-07-06 10:28:55 +0700

    first commit

---
+++ 04-upper-lower.dart
@@ -0,0 +1,8 @@
+void main() {
+  String title = 'Dart course';
+  print(title);
+  print(title.toUpperCase());
+  print(title);
+  print(title.toLowerCase());
+  print(title);
+}
---
+++ hello.dart
@@ -0,0 +1,23 @@
+
+
+void main() {
+  // Display
+  print('Hello Dart');
+  /* 
+  All
+  Comment
+  */
+  print("I Love"+'Dart Programming');
+  print("My Name is Sakchai\n I'm dev");
+  print(''' How to learn dart faster
+        way in this class
+        and go to production
+        in next time
+        ''');
+  print(r"This is \n multiline \n string");
+  int i =10;
+  double a =3.14;
+  print(a);
+  print(i);
+
+}
